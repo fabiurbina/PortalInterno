59d0d4ffdebbfc8559d4499decd5640bd0faf329
Fabio Urbina <fabiourbinasoares@gmail.com>
2026-07-21 11:06:11 -0300

    Alteração da linguagem para portugues

--- portal/settings.py
+++ portal/settings.py
@@ -125,14 +125,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
